3834df5233ca8bc2f1e5c1e439d5aad4030ff133
leapkh <leapkh@yahoo.com>
2018-10-24 20:27:18 +0700

    Request data from server.

---
+++ CKCC/Profile.swift
@@ -0,0 +1,19 @@
+//
+//  Profile.swift
+//  CKCC
+//
+//  Created by Bun Leap on 10/24/18.
+//  Copyright © 2018 Cambodia-Korea Cooperation Center. All rights reserved.
+//
+
+import Foundation
+
+struct Profile: Decodable {
+    
+    let name: String
+    let major: String
+    let gender: String
+    let dob: String
+    let imageUrl: String
+    
+}
---
+++ CKCC/ProfileTableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ProfileTableViewController.swift
+//  CKCC
+//
+//  Created by Bun Leap on 10/24/18.
+//  Copyright © 2018 Cambodia-Korea Cooperation Center. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewController: UITableViewController {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var majorLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var dobLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load profile from Web Service
+        let url = URL(string: "http://test.js-cambodia.com/ckcc/profile.php")!
+        //let url = URL(string: "http://localhost/test/ckcc/profile.php")!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            print("Request completed")
+            // Decode json data to object
+            let decoder = JSONDecoder()
+            
+            if let profile = try? decoder.decode(Profile.self, from: data!) {
+                DispatchQueue.main.async {
+                    self.nameLabel.text = profile.name
+                    self.majorLabel.text = profile.major
+                    self.genderLabel.text = profile.gender
+                    self.dobLabel.text = profile.dob
+                }
+            } else {
+                print("Decode data error...")
+            }
+        }
+        print("Start requesting to server...")
+        task.resume()
+    }
+
+}
